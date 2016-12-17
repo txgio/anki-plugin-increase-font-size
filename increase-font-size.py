cef3e788c889cd54500e8f19de348201a6a372b6
Tomás Godoi <tsmgodoi@gmail.com>
2016-12-17 21:57:09 -0200

    Adding the plugin file.

---
+++ increase-font-size.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright: Damien Elmes <anki@ichi2.net>
+# License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
+
+#FONT = "Times New Roman"
+
+from aqt import mw
+from aqt.qt import *
+
+FONT_SIZE = 12
+
+def changeGlobalFontSize():
+    font = QApplication.font()
+    font.setPixelSize(FONT_SIZE)
+    QApplication.setFont(font)
+
+def changeWebFontSize():
+    ws = QWebSettings.globalSettings()
+    ws.setFontSize(QWebSettings.DefaultFontSize, FONT_SIZE)
+
+def changeFontSize():
+    changeGlobalFontSize()
+    changeWebFontSize()
+
+changeFontSize()
